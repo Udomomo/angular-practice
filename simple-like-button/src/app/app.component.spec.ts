89a4c92177eb7788058b8dae1c422d028da65a69
Udomomo <udomomo@gmail.com>
2022-07-17 23:09:50 +0900

    create test for like button

--- simple-like-button/src/app/app.component.spec.ts
+++ simple-like-button/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('simple-like-button');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-like-button app is running!');
-  });
 });
--- simple-like-button/src/app/like-button/like-button.component.spec.ts
+++ simple-like-button/src/app/like-button/like-button.component.spec.ts
@@ -1,10 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { LikeButtonComponent } from './like-button.component';
 
 describe('LikeButtonComponent', () => {
   let component: LikeButtonComponent;
   let fixture: ComponentFixture<LikeButtonComponent>;
+  const counterPrefix = '♥ ';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,7 +19,26 @@ describe('LikeButtonComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have counter with value 0 when initialized', () => {
+    // Arrange
+    const el = fixture.debugElement.query(By.css('.likeButton')).nativeElement;
+    
+    // Verify
+    expect(el.textContent).toBe(counterPrefix + '0');
+  })
+
+
+  it('should increment counter by one when clicked', fakeAsync(() => {
+    // Arrange
+    const initEl = fixture.debugElement.query(By.css('.likeButton'));
+
+    // Do
+    initEl.triggerEventHandler('click', null);
+    tick();
+    fixture.detectChanges(); // Required to trigger data binding and recognize counter change
+
+    // Verify
+    const clickedEl = fixture.debugElement.query(By.css('.likeButton'));
+    expect(clickedEl.nativeElement.textContent).toBe(counterPrefix + '1');
+  }));
 });
