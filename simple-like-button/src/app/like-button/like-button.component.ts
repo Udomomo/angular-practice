1cb5d4ac0697d00ef2a2e31a9d5c3ae3812acf4e
Udomomo <udomomo@gmail.com>
2022-07-17 17:00:35 +0900

    create button component

---
+++ simple-like-button/src/app/like-button/like-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-like-button',
+  templateUrl: './like-button.component.html',
+  styleUrls: ['./like-button.component.scss']
+})
+export class LikeButtonComponent implements OnInit {
+  count = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
